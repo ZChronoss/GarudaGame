b7cbed0ddad6c865086fe6e1541397aee9eaafae
LeoLodra <leonardomarhan5@gmail.com>
2024-06-11 14:14:28 +0700

    Analog

--- GarudaGame/GameScene.swift
+++ GarudaGame/GameScene.swift
@@ -1,109 +1,93 @@
-//
-//  GameScene.swift
-//  GarudaGame
-//
-//  Created by Renaldi Antonio on 06/06/24.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    var player = SKSpriteNode()
+    var platform = SKShapeNode()
+    var leftButton = SKSpriteNode()
+    var rightButton = SKSpriteNode()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var joystickBase = SKShapeNode()
+    var joystickStick = SKShapeNode()
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    var joystickActive = false
+    var joystickStartPoint = CGPoint.zero
+    var playerVelocity = CGVector.zero
+    
+    override func didMove(to view: SKView) {
+        // Setup player
+        player = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        player.position = CGPoint(x: 0, y: 0)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.affectedByGravity = true
+        player.physicsBody?.restitution = 0
+        addChild(player)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Setup platform
+        platform = setupSprite(name: "a")
+        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.frame.size)
+        platform.physicsBody?.isDynamic = false
+        platform.physicsBody?.affectedByGravity = false
+        platform.physicsBody?.restitution = 0
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        // Setup joystick base
+        joystickBase = SKShapeNode(circleOfRadius: 100)
+        joystickBase.position = CGPoint(x: -size.width / 2 + 200, y: -size.height / 2 + 200)
+        joystickBase.alpha = 0.5
+        addChild(joystickBase)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Setup joystick stick
+        joystickStick = SKShapeNode(circleOfRadius: 30)
+        joystickStick.fillColor = .gray
+        joystickStick.position = joystickBase.position
+        addChild(joystickStick)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func setupSprite(name: String) -> SKShapeNode{
+        return self.childNode(withName: name) as! SKShapeNode
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            if joystickBase.frame.contains(location) {
+                joystickActive = true
+                joystickStartPoint = location
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        if joystickActive, let touch = touches.first {
+            let location = touch.location(in: self)
+            let offset = CGPoint(x: location.x - joystickStartPoint.x, y: location.y - joystickStartPoint.y)
+            let direction = CGVector(dx: offset.x, dy: offset.y)
+            let length = sqrt(direction.dx * direction.dx + direction.dy * direction.dy)
+            
+            // Limit the stick movement to the joystick base
+            let maxDistance: CGFloat = 50
+            let clampedDistance = min(length, maxDistance)
+            let clampedDirection = CGVector(dx: direction.dx / length * clampedDistance, dy: direction.dy / length * clampedDistance)
+            
+            joystickStick.position = CGPoint(x: joystickBase.position.x + clampedDirection.dx, y: joystickBase.position.y + clampedDirection.dy)
+            
+            // Calculate player velocity
+            playerVelocity = CGVector(dx: clampedDirection.dx * 0.1, dy: clampedDirection.dy * 0.2) // Adjust multiplier as needed
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if joystickActive {
+            joystickActive = false
+            joystickStick.position = joystickBase.position
+            playerVelocity = CGVector.zero
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        player.position.x += playerVelocity.dx
+        player.position.y += playerVelocity.dy
     }
 }
